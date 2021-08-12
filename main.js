a0508a9d5f1f08eb91ff1c27e5b76df66e6f2b9a
Gliga Dumitru <gliga_dumitru@yahoo.com>
2021-08-12 20:36:17 +0300

    feat: create option to auto generate game

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ const currentUser = {
   value: "",
 };
 
+const state = {
+  numberOfRows: 5,
+  gameArray: [],
+};
+
 function get(id) {
   return document.getElementById(id);
 }
@@ -16,17 +21,114 @@ function hide(id) {
 }
 
 function set(id, value) {
-    get(id).textContent = value;
+  get(id).textContent = value;
+}
+
+function setStageOne() {
+  hide("start-stage");
+  show("setup-stage-one");
+}
+
+function handleChangeNumberRows() {
+  let el = get("numberOrRows");
+  if (el.value >= 5 && el.value <= 10) {
+    state.numberOfRows = el.value;
+  } else {
+    window.blur();
+    state.numberOfRows = 5;
+    el.value = 5;
+    setAlert("", "The number must be between 5 and 10");
+  }
+
+  generateGameArray();
+  drawGame();
+}
+
+function setAlert(alertType = "", message = "", timeout = 5000) {
+  set("generalAlertText", message);
+  show("generalAlert");
+  setTimeout(() => {
+    hide("generalAlert");
+  }, timeout);
 }
 
 function loginWithUsername() {
   const { value } = get("username");
   currentUser.isOnline = true;
   currentUser.value = value;
+  set("userId", value);
+  setStageOne();
 
-  hide("start-stage");
-  show("setup-stage");
-  set('userId', value) 
-  // return false to preventDefault;
+  localStorage.setItem("usernameID", value);
   return false;
+}
+
+function logout() {
+  localStorage.removeItem("usernameID");
+  window.location.reload();
+}
+
+function generateGame() {
+  const gameContainer = get("gameContainer");
+  const generateGameArray = generateGameArray();
+  console.log(generateGameArray);
+}
+
+(function () {
+  const usernameID = localStorage.getItem("usernameID");
+  if (usernameID) {
+    set("userId", usernameID);
+    setStageOne();
+    generateGameArray();
+    drawGame();
+  }
+})();
+
+function drawGame() {
+  const gameContainer = get("game-container");
+  const { gameArray } = state;
+  let output = "";
+
+  gameArray.forEach((row) => {
+    let _tempLine = "";
+    row.forEach((cell) => {
+      let _tempCell = `
+        <div class="cell">
+            <input type="text" class="input-cell" />
+            <span class="numberId">1</span>
+        </div>
+      `;
+      _tempLine += _tempCell;
+    });
+
+    _tempLine = `
+      <div class="line">
+        ${_tempLine}
+      </div>
+    `;
+
+    output += _tempLine;
+  });
+
+  output = `
+   <form >
+      <section class="game-container">
+        ${output}
+      </section>
+  </form>
+  `;
+
+  gameContainer.innerHTML = output;
+}
+
+function generateGameArray() {
+  const { numberOfRows } = state;
+  let output = [];
+  const _tempCell = {
+    type: "default",
+    value: "",
+    disabled: false,
+  };
+  output = Array.from({ length: numberOfRows }, (v, k) => _tempCell);
+  state.gameArray = Array.from({ length: numberOfRows }, (v, k) => output);
 }
